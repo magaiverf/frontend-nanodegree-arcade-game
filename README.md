dd3ea24056a2ca878029dacab1b703775b2a46f0
magaiverf <magaiverf@gmail.com>
2017-04-05 20:37:01 -0300

    Create and finish the arcade game

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-frontend-nanodegree-arcade-game
-===============================
+frontend-nanodegree-arcade-game - Magaiver Oliveira
+===================================================
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
-
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+This project was created to complete the Javascript Oriented Object module in the [Udacity Nanodegree](https://br.udacity.com/course/front-end-web-developer-nanodegree--nd001/).
+* This is a simple game and your target is arrive safe in the water. 
+* Use the arrow keys to move in the game.
+* Watch out! The bugs can kill you!
--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+     this.x += this.speed * dt;
+
+    // make enemies loop to left side of canvas after reaching canvas.width
+    if (this.x >= 505) {
+        this.x = 0;
+    }    
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +33,51 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 400;
+    this.speed = 90;
+};
+
+Player.prototype.update = function() {
+
+};
+
+// Draw the player on the screen
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// handle the user input moving the player on the screen
+Player.prototype.handleInput = function(key) {
+    switch (key) {
+        case 'left':
+            if ((this.x - this.speed) > 0)
+                this.x -= this.speed;
+            break;    
+        case 'up':
+            if ((this.y - this.speed) > -100)
+                this.y -= this.speed;
+            break;    
+        case 'right':
+            if ((this.x + this.speed) < 400)
+                this.x += this.speed;
+            break;    
+        case 'down':
+            if ((this.y + this.speed) < 450)
+                this.y += this.speed;
+            break;    
+    }
+};
+
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var allEnemies = [new Enemy(1, 60, 100), new Enemy(1, 140, 200), new Enemy(1, 220, 300)];
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -43,4 +90,27 @@ document.addEventListener('keyup', function(e) {
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
+    checkVictory();
 });
+
+var checkVictory = function() {
+    if (player.y == -50) {
+        alert('YOU WINS!!!');
+        player = new Player();
+    }
+};
+
+var checkCollisions = function () {    
+    for (var index = 0; index < allEnemies.length; index++) {
+        var enemie = allEnemies[index];        
+
+        // verify the enemie's position + image size to confirm if there's a collision.
+        var collisonX = enemie.x + 70 >= player.x && enemie.x <= player.x + 70;
+        var collisonY = enemie.y >= player.y && enemie.y <= player.y + 85;
+
+        if (collisonX && collisonY) {
+            alert('YOU LOSE!!!');
+            player = new Player();
+        }
+    }
+};
